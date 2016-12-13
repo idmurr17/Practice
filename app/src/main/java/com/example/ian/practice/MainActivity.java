7cb32503a7ee1f4b72875b658a782896808520b1
Ian <idmurr17@gmail.com>
2016-12-13 18:13:18 -0500

    Test Push

--- app/src/main/java/com/example/ian/practice/MainActivity.java
+++ app/src/main/java/com/example/ian/practice/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // check push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
